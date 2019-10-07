b8ac74ed10643b64d7d8262b74e06a8f0f078002
Prasenjit Singh <prasenjit.singh@kore.com>
2019-10-07 12:58:54 +0530

    Error handling on db connection

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const logger = require('morgan');
 const apiRoutes = require('./routes/apiRoutes');
 
 const mongoose = require('mongoose');
- 
+
 mongoose.connect('mongodb://localhost/virtusaDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
+}).then(() => {
+  console.info('connetion to mondodb successfully done');
+}).catch((error) => {
+  console.error(error.message);
+  process.exit(0); 
 });
 
 var app = express();
@@ -25,12 +30,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   if (err) {
     res.status(404);
     res.json({
